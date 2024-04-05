593304e67bac9a733be19630e5773c5d43b5aa1d
richardadu-poku <richardjadupoku@gmail.com>
2024-04-05 17:38:34 -0400

    Send email once account is created

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
 
 # Instantiating the app
 app = Flask(__name__)
@@ -7,9 +8,17 @@ app = Flask(__name__)
 # Setting parameters
 app.config["SECRET_KEY"] = "youcannothackthis"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config['MAIL_SERVER'] = 'smtp-relay.brevo.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = "richardjadupoku@gmail.com"
+app.config['MAIL_PASSWORD'] = 'cTf3OHtSq5QxZ4nE'
 
 # Instantiating SQL
 db = SQLAlchemy(app)
+
+# Mail Instance
+mail = Mail(app)
 
 
 # Create Database class
@@ -53,12 +62,25 @@ def index():
             flash("There is an account with this email,"
                   " try a new email or sign in if you already have an account", "info")
 
-        # Only add data to database, if password and confirm password match
+        # Email body and structure
+        message_body = (f"{first_name}, \n \n"
+                        f"Welcome to the Eaze family. We are pleased that you chose to join us. \n \n"
+                        f"{username} is your username \n \n"
+                        f"Thank you!"
+                        )
+
+        message = Message(subject="Welcome to Eaze!",
+                          sender=app.config["MAIL_USERNAME"],
+                          recipients=[email],
+                          body=message_body)
+
+        # Only add data to database and send email, if password and confirm password match and there
+        # is no duplicate email or username
         if password == confirm_password and username not in username_entries and email not in email_entries:
             db.session.add(signup_form)
             db.session.commit()
+            mail.send(message)
 
-    # if email ==
     return render_template("index.html")
 
 
